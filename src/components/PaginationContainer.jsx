ac23ba075b9949a5534c5a51dd1cd07172dd3f30
Rentoshiro <149387302+Rentoshiro@users.noreply.github.com>
2024-02-23 20:45:43 +0100

    added Filters, pagination

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,65 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
+
 function PaginationContainer() {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  if (pageCount < 2) return null;
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage === 0) {
+              return;
+            }
+            handlePageChange(prevPage);
+          }}
+          className="btn btn-xs sm:btn-md join-vertical"
+        >
+          Prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page ? "bg-slate-400 border-base-300" : ""
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) {
+              return;
+            }
+            handlePageChange(nextPage);
+          }}
+          className="btn btn-xs sm:btn-md join-vertical"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default PaginationContainer;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,22 +1,23 @@
-import { useNavigation } from "react-router-dom";
 import { Filters, ProductsContainer, PaginationContainer } from "../components";
 import { customFetch } from "../utils";
 
 const url = "/products";
 
 export const loader = async ({ request }) => {
-  const response = await customFetch(url);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const response = await customFetch(url, {
+    params,
+  });
   const products = response.data.data;
   const meta = response.data.meta;
-  return { products, meta };
+  return { products, meta, params };
 };
 
 function Products() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
   return (
     <>
-      {isLoading && "loading..."}
       <Filters />
       <ProductsContainer />
       <PaginationContainer />
